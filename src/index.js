3dfc773319934354ba8974c2e133fbc8b5a8b0dc
Zhengyi Liang <zliang@atb.com>
2018-02-15 21:50:27 -0800

    Able to search for videos as the user enters the term

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -16,24 +17,29 @@ class App extends Component {
             selectedVideo: null
         };
 
-        YTSearch({key: API_KEY, term: 'surfboards'}, 
-                    (videos) => {
-                        this.setState({ 
-                            videos: videos,
-                            selectedVideo: videos[0]
-                        });
-                    });
+        this.videoSearch('surfboards')
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term }, 
+            (videos) => {
+                this.setState({ 
+                    videos: videos,
+                    selectedVideo: videos[0]
+                });
+        });
     }
 
     render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
         return ( 
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={videoSearch}/>
                 <VideoDetail video={this.state.selectedVideo}/>
                 <VideoList 
                     onVideoSelect={(selectedVideo) => {
                         this.setState({selectedVideo});
-                        console.log("hello world");
                     }}
                     videos={this. state.videos} />
             </div>
